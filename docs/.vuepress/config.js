a49a807f0301ce8b5a0b39e08c7e2eb7de2f6783
Zhu <406132631@qq.com>
2019-06-12 22:22:36 +0800

    init project

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: '前端打杂师 - Zhu',
+  description: 'All is well',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Blog', link: '/guide/' },
+      { text: 'GitHub', link: 'www.baidu.com'}
+    ]
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,7 @@
+---
+home: true
+heroImage: /image/avatar.jpeg
+actionText: My Blog →
+actionLink: /zh/guide/
+footer: 做人嘛 最重要的就是开心啦
+---
